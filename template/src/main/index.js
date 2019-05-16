8577fb09ac931e40a5d49fe34e0e298ff2139311
saikksub <saikksub@gmail.com>
2019-05-16 10:13:20 +0530

    update

--- template/src/main/index.js
+++ template/src/main/index.js
@@ -67,7 +67,11 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     height: 563,
     useContentSize: true,
-    width: 1000
+    width: 1000,
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: true
+    }
   })
 
   mainWindow.loadURL(winURL)
